183d7e46ae872473fa19601d332a3c9ea0f12da1
Paweł Walkowski <walkowski.pawel@wp.pl>
2020-03-24 13:39:10 +0100

    Add scripts: test:js: eslint

--- js/script.js
+++ js/script.js
@@ -51,12 +51,11 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-function generateTitleLinks(){
-
+function generateTitleLinks() {
+  
   /* remove contents of titleList, MODULE 5.4 */
 
   const titleList = document.querySelector(optTitleListSelector);
-
   titleList.innerHTML = '';
 
   /* for each article, MODULE 5.4 */
@@ -64,21 +63,21 @@ function generateTitleLinks(){
 
   const articles = document.querySelectorAll(optArticleSelector);
   console.log(articles);
-  
+
   let html = '';
 
-  for(let article of articles){
+  for (let article of articles) {
 
     /* get the article id, MODULE 5.4 */
-
+    
     const articleId = article.getAttribute('id');
     console.log(articleId);
 
     /* find the title element, MODULE 5.4 */
     /* get the title from the title element, MODULE 5.4 */
-
+    
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
+    
     /* create HTML of the link, MODULE 5.4 */
 
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
@@ -90,14 +89,13 @@ function generateTitleLinks(){
     
     html = html + linkHTML;
   }
-
   titleList.innerHTML = html;
 }
 
 generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
-console.log(links)
+console.log(links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
